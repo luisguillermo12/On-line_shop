da2d52696d74b3842ad28770261b46b99230563b
lvalles <luisguillermo12>
2019-02-25 12:18:18 -0400

    menu home

--- resources/views/layouts/siderbar.blade.php
+++ resources/views/layouts/siderbar.blade.php
@@ -25,9 +25,15 @@
                 </div>
           </div>
       </div>
-                      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav navbar-right">
-                     </div>
+                       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                      <ul class="nav navbar-nav">
+   
+                
+                              <li> <a href="{{url ('/')  }}"> home </a> </li>
+                          
+
+
+    </div>
   </div>
 </nav>
 
